e9731750bd28029982b3ec24f20b8166c701199a
David <daviddoll@users.noreply.github.com>
2026-03-23 10:16:08 +0100

    Update checkmk_search_keyword_on_website.ps1
    fixed typo

--- checkmk_search_keyword_on_website.ps1
+++ checkmk_search_keyword_on_website.ps1
@@ -1,21 +1,21 @@
 ### local check for Checkmk
 ### Exits with status "0" if the keyword is found on the website and "1" if the keyword is not found
 
-### Date of last change: 2023-06-06
-### Version 0.1
+### Date of last change: 2026-03-23
+### Version 0.2
 
 $website = "https://www.cnn.com"
 $cfa = Invoke-WebRequest -Uri $website
 $searchkeyword = $cfa.tostring() -split "[`r`n]" | select-string "Trump"
 
-if (!$searchkeyword)
+if ($searchkeyword)
     {
-        $status = "1"
+        $status = "0"
         $statusdescription = "He is still here!!"
     } 
 else 
     {
-        $status = "0"
+        $status = "1"
         $statusdescription = "He is gone ..." 
     }
 
